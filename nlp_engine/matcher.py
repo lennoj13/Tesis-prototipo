ddadfb8a0f2c5d0f44564837ca24f109e4673804
unknown <galarzabryan13@outlook.com>
2026-06-13 01:25:53 -0500

    nuevos modelos similitudCoseno y add nueva version recomendacionhibridaComponent que los use

--- nlp_engine/matcher.py
+++
@@ -1,6 +0,0 @@
-# TODO: Matching bidireccional
-# - Similitud del coseno entre vectores estudiante ↔ vacante
-# - Cálculo de porcentaje de afinidad
-# - Ranking de recomendaciones bidireccional
-#
-# Librería: scikit-learn (cosine_similarity), numpy
