d986889af8ce707c14c735d072feb147b57a6d37
lennoj13 <jonnelanchundia@gmail.com>
2026-06-22 14:31:10 -0500

    .

--- backend/scratch_test.py
+++
@@ -1,9 +0,0 @@
-def response_success():
-    return {'data': []}, 200
-
-try:
-    resp = response_success()
-    resp[0]['is_calculating'] = True
-    print("Success:", resp)
-except Exception as e:
-    print("Error:", e)
